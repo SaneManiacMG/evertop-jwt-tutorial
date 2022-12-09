00d472ad49c0ca88739ae29fcd3019b2c51b1f95
SaneManiacMG <mmoroane@hotmail.com>
2022-12-09 11:59:58 +0200

    Imported Java code

---
+++ springsecurityjwt/src/test/java/com/evertop/spring/security/springsecurityjwt/SpringsecurityjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertop.spring.security.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
